fc61e706d04294aa2458997c276078bde03035c2
Miliexe <emilie.lblc@gmail.com>
2023-10-12 05:38:20 +0200

    Init Header

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,7 +1,56 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import SearchIcon from '@mui/icons-material/Search'
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
+import './Header.scss'
 
 const Header = () => {
-    return <div className="header"></div>
+    return (
+        <div className="header">
+            <div className="wrapper">
+                <Link className="link" to="/">
+                    <div className="logo">
+                        <img src="img/logo.jpg" alt="" />
+                    </div>
+                </Link>
+                <div className="navbar">
+                    <Link className="link" to="/">
+                        <div className="navbar-item">Bijoux</div>
+                    </Link>
+                    <Link className="link" to="/">
+                        <div className="navbar-item">Personnalisation</div>
+                    </Link>{' '}
+                    <Link className="link" to="/">
+                        <div className="navbar-item">Totebags</div>
+                    </Link>
+                    <Link className="link" to="/">
+                        <div className="navbar-item">Stickers</div>
+                    </Link>
+                    <Link className="link" to="/">
+                        <div className="navbar-item">La marque</div>
+                    </Link>
+                    <Link className="link" to="/">
+                        <div className="navbar-item navbar-item--arrow">
+                            Aide
+                            <KeyboardArrowDownIcon />
+                        </div>
+                    </Link>
+                    <Link className="link" to="/">
+                        <div className="navbar-item">Contact</div>
+                    </Link>
+                    <Link className="link" to="/">
+                        <div className="navbar-item">Mon compte</div>
+                    </Link>{' '}
+                    <Link className="link" to="/">
+                        <div className="navbar-item">Panier</div>
+                    </Link>
+                    <div className="icons">
+                        <SearchIcon />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Header
