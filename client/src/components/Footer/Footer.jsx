ad499a271080c003888d88d2418e5047e3e37461
Miliexe <emilie.lblc@gmail.com>
2023-10-14 20:03:15 +0200

    Init Footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,7 +1,36 @@
 import React from 'react'
+import './Footer.scss'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
-    return <div className="footer"></div>
+    return (
+        <div className="footer">
+            <div className="container">
+                <div className="footer-top">
+                    <ul className="items">
+                        <Link className="link" to="/">
+                            <li className="item">Mentions légales</li>
+                        </Link>
+                        <Link className="link" to="/">
+                            <li className="item">
+                                Conditions générales de vente
+                            </li>
+                        </Link>
+                        <Link className="link" to="/">
+                            <li className="item">
+                                Politique de confidentialité
+                            </li>
+                        </Link>
+                    </ul>
+                </div>
+                <div className="footer-bot">
+                    <ul className="items">
+                        <li className="item">Ici c'est sympa © 2022</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Footer
