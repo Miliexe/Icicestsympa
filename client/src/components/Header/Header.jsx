b78fc821fc90562aba9b30c3c68134c51d3e3051
Miliexe <emilie.lblc@gmail.com>
2023-10-15 00:42:22 +0200

    Update Header
    fix responsive issue

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -14,39 +14,41 @@ const Header = () => {
                     </div>
                 </Link>
                 <div className="navbar">
-                    <Link className="link" to="/">
-                        <div className="navbar-item">Bijoux</div>
-                    </Link>
-                    <Link className="link" to="/">
-                        <div className="navbar-item">Personnalisation</div>
-                    </Link>{' '}
-                    <Link className="link" to="/">
-                        <div className="navbar-item">Totebags</div>
-                    </Link>
-                    <Link className="link" to="/">
-                        <div className="navbar-item">Stickers</div>
-                    </Link>
-                    <Link className="link" to="/">
-                        <div className="navbar-item">La marque</div>
-                    </Link>
-                    <Link className="link" to="/">
-                        <div className="navbar-item navbar-item--arrow">
-                            Aide
-                            <KeyboardArrowDownIcon className="icons" />
-                        </div>
-                    </Link>
-                    <Link className="link" to="/">
-                        <div className="navbar-item">Contact</div>
-                    </Link>
-                    <Link className="link" to="/">
-                        <div className="navbar-item">Mon compte</div>
-                    </Link>{' '}
-                    <Link className="link" to="/">
-                        <div className="navbar-item">Panier</div>
-                    </Link>
-                    <div className="icons">
-                        <SearchIcon />
-                    </div>
+                    <ul className="items">
+                        <Link className="link" to="/">
+                            <li className="item">Bijoux</li>
+                        </Link>
+                        <Link className="link" to="/">
+                            <li className="item">Personnalisation</li>
+                        </Link>{' '}
+                        <Link className="link" to="/">
+                            <li className="item">Totebags</li>
+                        </Link>
+                        <Link className="link" to="/">
+                            <li className="item">Stickers</li>
+                        </Link>
+                        <Link className="link" to="/">
+                            <li className="item">La marque</li>
+                        </Link>
+                        <Link className="link" to="/">
+                            <li className="item item--arrow">
+                                Aide
+                                <KeyboardArrowDownIcon className="icons" />
+                            </li>
+                        </Link>
+                        <Link className="link" to="/">
+                            <li className="item">Contact</li>
+                        </Link>
+                        <Link className="link" to="/">
+                            <li className="item">Mon compte</li>
+                        </Link>{' '}
+                        <Link className="link" to="/">
+                            <li className="item">Panier</li>
+                        </Link>
+                        <li className="icons">
+                            <SearchIcon />
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
